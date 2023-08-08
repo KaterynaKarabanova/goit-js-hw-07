6bf6c8ec82fcffad9680d79f0f37a409c58b75d2
Kateryna <ikatya7575@gmaiil.com>
2023-08-08 18:50:53 +0300

    Update js/01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,15 +3,13 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const galleryList = document.querySelector(".gallery")
-const markup = galleryItems.map(({original, preview, description})=> 
-  ` <li class="gallery__item">
+const markup = galleryItems.map(({original, preview, description})=> ` <li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
-      
     />
   </a>
 </li>`
@@ -26,23 +24,15 @@ function onGalleryListClick(event) {
         return
     }
     const imageSource = target.dataset.source
-    const currentImage = galleryItems.find(({ original }) => original === imageSource)
-    const instance = basicLightbox.create(`
-    <div>
-     <a class="gallery__link" href="${currentImage.original}">
-    <img
-      class="gallery__image"
-      src="${currentImage.original}"
-      alt="${currentImage.description}"
-    />
-  </a>
-    </div>
-`)
+    const instance = basicLightbox.create(`<img  class="gallery__image" src="${imageSource}" />`)
     instance.show()
-galleryList.addEventListener('keydown', function closeModal(event) {
-    if (event.key === 'Escape') {
+  galleryList.addEventListener('keydown', closeModal)
+
+  function closeModal(event) {
+  if (event.key === 'Escape') {
+    galleryList.removeEventListener('keydown', closeModal) 
     instance.close() 
   }
-});
+}
 }
 
