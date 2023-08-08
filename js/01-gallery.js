beb21bf3e0212bcc54daa31e6407f62f9a1477ce
Kateryna <ikatya7575@gmaiil.com>
2023-08-08 18:37:20 +0300

    made 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery")
+const markup = galleryItems.map(({original, preview, description})=> 
+  ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      
+    />
+  </a>
+</li>`
+)
+galleryList.insertAdjacentHTML(`beforeend`, markup.join(``))
+
+galleryList.addEventListener("click", onGalleryListClick)
+function onGalleryListClick(event) {
+    event.preventDefault();
+    const {target} = event
+    if (!event.target.classList.contains("gallery__image")) {
+        return
+    }
+    const imageSource = target.dataset.source
+    const currentImage = galleryItems.find(({ original }) => original === imageSource)
+    const instance = basicLightbox.create(`
+    <div>
+     <a class="gallery__link" href="${currentImage.original}">
+    <img
+      class="gallery__image"
+      src="${currentImage.original}"
+      alt="${currentImage.description}"
+    />
+  </a>
+    </div>
+`)
+    instance.show()
+galleryList.addEventListener('keydown', function closeModal(event) {
+    if (event.key === 'Escape') {
+    instance.close() 
+  }
+});
+}
+
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -5,14 +5,14 @@ export const galleryItems = [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
-  },
+  } ,
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
+    description: 'Container Haulage Freight' ,
+  } ,
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
